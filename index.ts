375c7eefbf3e8307e4ba43b5dc9ae10b92651f03
AidXylelele <warboyka48@gmail.com>
2023-07-29 21:42:49 +0200

    fix: Fixed imports

--- index.ts
+++ index.ts
@@ -1,7 +1,2 @@
-import { Storage } from "./src/utils/storage.util";
-import { OptionsBuilder } from "./src/utils/options.util";
-
-export default {
-  Storage,
-  OptionsBuilder,
-};
+export { Storage } from "./src/utils/storage.util";
+export { OptionsBuilder } from "./src/utils/options.util";
