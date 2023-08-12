80c79732843ec78d652a9378cd15210e90243052
AIDXYLELELE <warboyka48@gmail.com>
2023-08-12 09:52:30 +0200

    fix: Imporeted new tool

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,9 +1,11 @@
 export { InsertDBOptions, GetDBOptions, UpdateDBOptions, DeleteDBOptions, } from "./types/database.types";
 export { InsertCacheOptions, GetCacheOptions, DeleteCacheOptions, UpdateCacheOptions, } from "./types/cache.types";
+export { ResponseConstructor } from "./constructors/response.constructor";
+export { ErrorConstructor } from "./constructors/error.constructor";
+export { StorageRequestBuilder } from "./constructors/storage-request.constructor";
 export { Identification } from "./types/middlewares.types";
 export { ErrorHandler } from "./utils/error-handler.util";
 export { CacheableStorage } from "./services/storage.service";
-export { StorageRequestBuilder } from "./constructors/storage-request.constructor";
 export { validate } from "./middlewares/validate.middleware";
 export { errorMiddleware } from "./middlewares/error.middleware";
 export { authenticate } from "./middlewares/auth.middleware";
--- src/index.ts
+++ src/index.ts
@@ -10,10 +10,15 @@ export {
   DeleteCacheOptions,
   UpdateCacheOptions,
 } from "./types/cache.types";
+
+export { ResponseConstructor } from "./constructors/response.constructor";
+export { ErrorConstructor } from "./constructors/error.constructor";
+export { StorageRequestBuilder } from "./constructors/storage-request.constructor";
+
 export { Identification } from "./types/middlewares.types";
 export { ErrorHandler } from "./utils/error-handler.util";
 export { CacheableStorage } from "./services/storage.service";
-export { StorageRequestBuilder } from "./constructors/storage-request.constructor";
+
 export { validate } from "./middlewares/validate.middleware";
 export { errorMiddleware } from "./middlewares/error.middleware";
 export { authenticate } from "./middlewares/auth.middleware";
